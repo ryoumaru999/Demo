8fe24b3fbcffe34b5565b0ea14061e591e270751
ryoumaru999 <gujame0005@gmail.com>
2026-01-30 00:25:08 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll("details").forEach((detail) => {
-  detail.addEventListener("toggle", () => {
-    if (detail.open) {
-      document.querySelectorAll("details").forEach((d) => {
-        if (d !== detail) d.removeAttribute("open");
-      });
-    }
-  });
-});
